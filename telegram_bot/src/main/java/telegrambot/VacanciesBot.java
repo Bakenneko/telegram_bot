0c08a36b4ca77ff3f98f8af65507207cf3e07490
Andrew Kurylyk <andronniy7@gmail.com>
2023-05-11 14:56:22 +0300

    Second

--- telegram_bot/src/main/java/telegrambot/VacanciesBot.java
+++ telegram_bot/src/main/java/telegrambot/VacanciesBot.java
@@ -2,7 +2,15 @@ package telegrambot;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class VacanciesBot extends TelegramLongPollingBot {
@@ -16,7 +24,79 @@ public class VacanciesBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println("event received");
+        if (update.getMessage() != null) {
+            handleStartCommand(update);
+        }
+        if (update.getCallbackQuery() != null){
+            String callbackData = update.getCallbackQuery().getData();
+            if ("showJuniorVacancies".equals(callbackData)){
+                showJuniorVacancies(update);
+            }
+        }
+    }
+
+    private void showJuniorVacancies(Update update) {
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setText("Please choose vacancy:");
+        sendMessage.setChatId(update.getCallbackQuery().getMessage().getChatId());
+        sendMessage.setReplyMarkup(getJuniorVacanciesMenu());
+
+    }
+
+    private ReplyKeyboard getJuniorVacanciesMenu() {
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        InlineKeyboardButton maVacancy = new InlineKeyboardButton();
+        maVacancy.setText("Junior Java developer at MA");
+        maVacancy.setCallbackData("vacancyId=1");
+        row.add(maVacancy);
+
+        InlineKeyboardButton googleVacancy = new InlineKeyboardButton();
+        googleVacancy.setText("Junior Dev at google");
+        googleVacancy.setCallbackData("vacancyId=2");
+        row.add(googleVacancy);
+
+        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
+        keyboard.setKeyboard(List.of(row));
+        return keyboard;
+    }
+
+    private void handleStartCommand (Update update) {
+
+        String text = update.getMessage().getText();
+        System.out.println("Received text is "+ text);
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(update.getMessage().getChatId());
+        sendMessage.setText("Welcome to vacancies bot! Please choose your title:");
+        sendMessage.setReplyMarkup(getStartMenu());
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ReplyKeyboard getStartMenu() {
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        InlineKeyboardButton junior = new InlineKeyboardButton();
+        junior.setText("Junior");
+        junior.setCallbackData("showJuniorVacancies");
+        row.add(junior);
+
+        InlineKeyboardButton middle = new InlineKeyboardButton();
+        middle.setText("Middle");
+        middle.setCallbackData("showMiddleVacancies");
+        row.add(middle);
+
+        InlineKeyboardButton senior = new InlineKeyboardButton();
+        senior.setText("Senior");
+        senior.setCallbackData("showSeniorVacancies");
+        row.add(senior);
+
+        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
+        keyboard.setKeyboard(List.of(row));
+        return keyboard;
     }
 
     @Override
